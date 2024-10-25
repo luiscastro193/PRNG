83e54e4f3ebe1a11d1f56f092d841b2ab9f26a90
luiscastro193 <luiscastro193@gmail.com>
2024-10-26 01:45:12 +0200

    Only pre-compile module

--- fastPRNG.js
+++ fastPRNG.js
@@ -1,6 +1,6 @@
 "use strict";
 const maxValue = 4294967296;
-const wasm = fetch("pcg_prng.wasm").then(response => WebAssembly.instantiateStreaming(response)).then(result => result.module);
+const wasm = fetch("pcg_prng.wasm").then(response => WebAssembly.compileStreaming(response));
 
 async function toBigInt(seed) {
 	let hash = new TextEncoder().encode(seed);
