7acd7244910511132639185b2ee8d0dd1c3cedb4
luiscastro193 <luiscastro193@gmail.com>
2024-10-27 13:47:33 +0100

    Leverage whole space

--- PRNG.js
+++ PRNG.js
@@ -2,21 +2,15 @@
 const maxValue = 4294967296;
 const wasm = fetch(new URL("pcg_prng.wasm", import.meta.url)).then(response => WebAssembly.compileStreaming(response));
 
-async function toBigInt(seed) {
-	let hash = new TextEncoder().encode(seed);
-	
-	do {
-		hash = await crypto.subtle.digest("SHA-256", hash);
-		seed = new DataView(hash).getBigUint64();
-	} while (seed == 0n);
-	
-	return seed;
+async function toBigInts(seed) {
+	seed = new DataView(await crypto.subtle.digest("SHA-256", new TextEncoder().encode(seed)));
+	return [seed.getBigUint64(), seed.getBigUint64(8)];
 }
 
 export default async function PRNG(seed) {
-	seed = toBigInt(seed);
+	seed = toBigInts(seed);
 	let instance = wasm.then(module => WebAssembly.instantiate(module));
 	[seed, instance] = await Promise.all([seed, instance]);
-	instance.exports.pcg32_srandom(seed, seed);
+	instance.exports.pcg32_srandom(...seed);
 	return () => (instance.exports.pcg32_next() >>> 0) / maxValue;
 }
