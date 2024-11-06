dd8be8c5c457a15d07975ae077114b3cf87dae66
luiscastro193 <luiscastro193@gmail.com>
2024-11-06 01:16:34 +0100

    Inline wasm

--- PRNG.js
+++ PRNG.js
@@ -1,13 +1,7 @@
 "use strict";
-async function fetchWasm(resource) {
-	if (typeof process != 'undefined' && process?.versions?.undici)
-		return new Response(await (await import('node:fs')).openAsBlob(resource, {type: 'application/wasm'}));
-	else
-		return fetch(resource);
-}
-
+const wasmSource = "AGFzbQEAAAABCgJgAn5+AGAAAX8DAwIAAQYLAn4BQgALfgFCAAsHHgINcGNnMzJfc3JhbmRvbQAACnBjZzMyX25leHQAAQpYAh0AQgAkACABQgGGQgGEJAEQARojACAAfCQAEAEaCzgCAX4CfyMAIQAgAEKt/tXk1IX9qNgAfiMBfCQAIABCEoggAIVCG4inIQEgAEI7iKchAiABIAJ4Cw==";
+const wasm = WebAssembly.compile(Uint8Array.from(atob(wasmSource), c => c.codePointAt(0)));
 const maxValue = 4294967296;
-const wasm = WebAssembly.compileStreaming(fetchWasm(new URL("pcg_prng.wasm", import.meta.url)));
 
 async function toBigInts(seed) {
 	seed = new DataView(await crypto.subtle.digest("SHA-256", new TextEncoder().encode(seed)));
