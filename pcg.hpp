78ba0a48f5d4319f912decbf062d19650f469b74
luiscastro193 <luiscastro193@gmail.com>
2026-01-28 11:57:17 +0100

    Simplify code
    In order to closely match the referenced implementation. Output is the same.

--- pcg.hpp
+++ pcg.hpp
@@ -16,25 +16,21 @@ public:
 	pcg64_dxsm(uint64_t state_hi, uint64_t state_lo, uint64_t inc_hi, uint64_t inc_lo) noexcept:
 		state_{ (__uint128_t(state_hi) << 64) | __uint128_t(state_lo) },
 		inc_{ (__uint128_t(inc_hi) << 64) | __uint128_t(inc_lo) } {
-		inc_ = (inc_ << 1) | __uint128_t(1);
+		inc_ = (inc_ << 1) | 1;
 		state_ += inc_;
-		(void)(*this)();
+		(*this)();
 	}
 	
 	result_type operator()() noexcept {
 		constexpr uint64_t mul = 15750249268501108917ULL;
-		
 		const __uint128_t state = state_;
 		state_ = state * mul + inc_;
-		
 		uint64_t hi = uint64_t(state >> 64);
-		const uint64_t lo = uint64_t(state) | 1ULL;
-		
+		const uint64_t lo = uint64_t(state) | 1;
 		hi ^= hi >> 32;
 		hi *= mul;
 		hi ^= hi >> 48;
 		hi *= lo;
-		
 		return hi;
 	}
 };
