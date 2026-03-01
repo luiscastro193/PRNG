cc19aaf70fc942646177c5cafbcf073d2d8ad1de
luiscastro193 <luiscastro193@gmail.com>
2026-03-01 11:48:48 +0100

    Ensure determinism
    Since load time is highly optimized now anyway

--- compile.sh
+++ compile.sh
@@ -3,8 +3,7 @@ em++ random.cpp -I. \
 	-Oz -flto -fno-exceptions -fno-rtti -DNDEBUG \
 	-mtail-call -msimd128 -mavx2 \
 	-sENVIRONMENT=web -sEXPORT_ES6=1 --no-entry \
-	-sSTRICT=1 -sJS_MATH=1 \
-	--closure 1 -sEXPORT_KEEPALIVE=1 \
+	-sSTRICT=1 --closure 1 -sEXPORT_KEEPALIVE=1 \
 	-sMINIMAL_RUNTIME=1 -sMINIMAL_RUNTIME_STREAMING_WASM_INSTANTIATION=1 \
 	-sMALLOC=emmalloc -sINITIAL_HEAP=65536 \
 	-sALLOW_MEMORY_GROWTH=1 -sMEMORY_GROWTH_LINEAR_STEP=65536 \
